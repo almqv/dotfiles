3d5acbe391ec0959c6cdd0534e4061715997fa7d
E. Almqvist <h1g.server@gmail.com>
2020-07-21 21:20:01 +0200

    Updates

--- polybar/scripts/cmus.sh
+++ polybar/scripts/cmus.sh
@@ -6,6 +6,7 @@ prepend_zero () {
 
 artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
 
+if [[ $artist = *[!\ ]* ]]; then
         song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
         position=$(cmus-remote -C status | grep position | cut -c 10-)
         minutes1=$(prepend_zero $(($position / 60)))
@@ -14,3 +15,6 @@ artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
         minutes2=$(prepend_zero $(($duration / 60)))
         seconds2=$(prepend_zero $(($duration % 60)))
         echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
+else
+        echo
+fi
