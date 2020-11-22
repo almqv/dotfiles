16f30ecead330e08ba588fcf903437f5bd2a7768
E. Almqvist <elalmqvist@gmail.com>
2020-11-22 10:45:16 +0100

    Removed i3

--- copydots.sh
+++ copydots.sh
@@ -3,7 +3,6 @@
 repoPath=$HOME/dotfiles
 
 # All those things
-cp -r $HOME/.config/i3 $repoPath
 cp -r $HOME/.config/rofi $repoPath
 cp -r $HOME/.config/conky $repoPath
 cp -r $HOME/.config/polybar $repoPath
