cb11acd443fa80c5181cf5fd7d2d0d06c0db95ab
E. Almqvist <elalmqvist@gmail.com>
2021-05-10 19:45:49 +0200

    Minor changes

--- luke-st_config.h
+++ luke-st_config.h
@@ -107,8 +107,8 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-/* bg opacity */
-float alpha = 0.82;
+/* bg opacity 0.82*/
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
