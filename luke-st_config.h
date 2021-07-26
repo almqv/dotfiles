7f568dd569add63c60436dca5072e8516a949aac
E. Almqvist <elalmqvist@gmail.com>
2021-07-26 22:18:42 +0200

    Luke ST config update

--- luke-st_config.h
+++ luke-st_config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "Fira Code:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 20;
 
