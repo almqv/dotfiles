0e7a53ff6f4aa025dfa1938c6fe0e7b22fc0faf5
E. Almqvist <elalmqvist@gmail.com>
2021-08-17 15:14:09 +0200

    aliases

--- oh-my-zsh/.oh-my-zsh/aliases.sh
+++ oh-my-zsh/.oh-my-zsh/aliases.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/bash
 
-alias dev="cd ~/Projects"
-alias gmad="/home/elal/.local/share/Steam/steamapps/common/GarrysMod/bin/linux64/gmad"
-alias rasp="ssh pi@192.168.1.19" 
-alias srv1="ssh almtech@192.168.1.187 -p 2022"
-
 alias xcopy="xclip -selection c"
 alias xpaste="xclip -selection c -o"
 
 alias ccd="pwd | xcopy"
 
 # Programs
-alias asmcomp="/home/elal/Programs/asmcompiler/x86_64.sh"
 alias vidconvert="ffmpeg -i"
