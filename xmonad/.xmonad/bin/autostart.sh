1bbba1fcec5c8a8867df32d3199aaee48888e12b
E. Almqvist <elalmqvist@gmail.com>
2022-10-16 00:48:57 +0200

    XMonad & polybar update

---
+++ xmonad/.xmonad/bin/autostart.sh
@@ -0,0 +1,30 @@
+# Keyboard layout
+setxkbmap se &
+
+# Display settings
+$HOME/.screenlayout/layout.sh &
+
+# Compositor
+$HOME/.config/picom/launch &
+
+# Background
+nitrogen --restore &
+
+# Notification daemon
+dunst &
+
+# Anti sleep
+xset s off &
+xset -dpms &
+
+# EWW widgets etc
+#eww daemon
+#eww open bar
+
+# Polybar
+$HOME/.config/polybar/launch &
+
+# Keyring
+eval $(gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
+
