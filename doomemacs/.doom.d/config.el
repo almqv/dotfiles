db41608fee0133e1f18df43bbf0348ef09771617
Elias Almqvist <elalmqvist@gmail.com>
2022-11-30 10:36:27 +0100

    Merge fix

--- doomemacs/.doom.d/config.el
+++ doomemacs/.doom.d/config.el
@@ -74,3 +74,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Indentation
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode t)
