75224694ad7c06aa4cb9d565b6eed51e65e42a0f
Elias Almqvist <elalmqvist@gmail.com>
2022-11-30 10:28:06 +0100

    Workspaces for emacs

--- doomemacs/.doom.d/init.el
+++ doomemacs/.doom.d/init.el
@@ -49,7 +49,7 @@
        ;; vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
-       ;;workspaces        ; tab emulation, persistence & separate workspaces
+       workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
        :editor
@@ -95,7 +95,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
