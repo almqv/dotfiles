324954eb5b4c8eb8f2dd78f042b67d60d7d53e62
E. Almqvist <elalmqvist@gmail.com>
2022-12-19 17:24:41 +0100

    LSP stuff & gitignore for Emacs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -38,7 +38,7 @@
  '(custom-safe-themes
    '("7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
  '(package-selected-packages
-   '(treemacs-evil treemacs projectile doom-themes lsp-mode evil)))
+   '(go-mode rust-mode use-package treemacs-evil treemacs projectile doom-themes lsp-mode evil)))
     
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -46,6 +46,43 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; LSP
+(require 'use-package)
+(use-package lsp-mode
+  :hook ((c-mode          ; clangd
+          c++-mode        ; clangd
+          c-or-c++-mode   ; clangd
+          java-mode       ; eclipse-jdtls
+          js-mode         ; ts-ls (tsserver wrapper)
+          js-jsx-mode     ; ts-ls (tsserver wrapper)
+          typescript-mode ; ts-ls (tsserver wrapper)
+          python-mode     ; pyright
+          web-mode        ; ts-ls/HTML/CSS
+          haskell-mode    ; haskell-language-server
+	  lisp-mode       ;
+	  rust-mode       ;
+	  go-mode         ;
+          ) . lsp-deferred)
+  :commands lsp
+  :config
+  (setq lsp-auto-guess-root t)
+  (setq lsp-log-io nil)
+  (setq lsp-restart 'auto-restart)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-render-documentation nil)
+  (setq lsp-eldoc-hook nil)
+  (setq lsp-modeline-code-actions-enable nil)
+  (setq lsp-modeline-diagnostics-enable nil)
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-semantic-tokens-enable nil)
+  (setq lsp-enable-folding nil)
+  (setq lsp-enable-imenu nil)
+  (setq lsp-enable-snippet nil)
+  (setq read-process-output-max (* 1024 1024)) ;; 1MB
+  (setq lsp-idle-delay 0.5))
 
 ;; Loading config stuff
 (load "$HOME/.config/emacs/config.el")
