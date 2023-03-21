e1aa1327f88aefcebeb44d53317b927f767bdfb9
E. Almqvist <elalmqvist@gmail.com>
2023-03-21 13:28:15 +0100

    Minor tweaks

--- doomemacs/.doom.d/custom.el
+++ doomemacs/.doom.d/custom.el
@@ -3,10 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(cpputils-cmake eldoc-cmake quelpa elcord)))
+ '(elcord-display-buffer-details nil)
+ '(elcord-display-elapsed t)
+ '(elcord-display-line-numbers nil)
+ '(elcord-editor-icon "emacs_material_icon")
+ '(elcord-icon-base
+   "https://raw.githubusercontent.com/Mstrodl/elcord/master/icons/")
+ '(elcord-idle-message "Thinking...")
+ '(elcord-mode t nil (elcord))
+ '(elcord-quiet t)
+ '(package-selected-packages '(elcord quelpa eldoc-cmake cpputils-cmake))
+ '(warning-suppress-types '((initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'customize-group 'disabled nil)
