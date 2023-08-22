e06226519007bd5b4b0d8ae9e6ec674eb71f67e4
Elias Almqvist <elalmqvist@gmail.com>
2023-08-22 20:55:31 +0200

    Packer

--- nvim/.config/nvim/lua/evim/packer.lua
+++ nvim/.config/nvim/lua/evim/packer.lua
@@ -16,7 +16,7 @@ vim.cmd [[packadd packer.nvim]]
 return require("packer").startup(function(use)
 	-- Core
 	use "wbthomason/packer.nvim"
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	-- use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 	use {
 		"nvim-telescope/telescope.nvim", tag = "0.1.1",
 		requires = { { "nvim-lua/plenary.nvim" } }
