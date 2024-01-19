f0572acb275feb48bccb7f878ca9121af6e048b7
Elias Almqvist <elalmqvist@gmail.com>
2024-01-19 21:08:54 +0100

    feat: NvChad binds

--- nvchad/.config/nvim/lua/custom/configs/conform.lua
+++ nvchad/.config/nvim/lua/custom/configs/conform.lua
@@ -1,28 +1,33 @@
 local options = {
-  lsp_fallback = true,
+	lsp_fallback = true,
 
-  formatters_by_ft = {
-    -- Divine Intellect Languages
-    lua = { "stylua" },
+	formatters_by_ft = {
+		-- Divine Intellect Languages
+		lua = { "stylua" },
 
-    -- Web Dev shit
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    css = { "prettier" },
-    scss = { "prettier" },
-    html = { "prettier" },
+		-- Web Dev shit
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		css = { "prettier" },
+		scss = { "prettier" },
+		html = { "prettier" },
 
-    sh = { "shfmt" },
-  },
+		-- Data Science & AI stuff
+		python = { "black" },
 
-  -- adding same formatter for multiple filetypes can look too much work for some
-  -- instead of the above code you could just use a loop! the config is just a table after all!
+		sh = { "shfmt" },
+	},
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+	-- adding same formatter for multiple filetypes can look too much work for some
+	-- instead of the above code you could just use a loop! the config is just a table after all!
+
+	-- format_on_save = {
+	--   -- These options will be passed to conform.format()
+	--   timeout_ms = 500,
+	--   lsp_fallback = true,
+	-- },
 }
 
 require("conform").setup(options)
--- nvchad/.config/nvim/lua/custom/mappings.lua
+++ nvchad/.config/nvim/lua/custom/mappings.lua
@@ -2,21 +2,26 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-    --  format with conform
-    ["<leader>fm"] = {
-      function()
-        require("conform").format()
-      end,
-      "formatting",
-    }
+		--  format with conform
+		["<leader>fm"] = {
+			function()
+				require("conform").format()
+			end,
+			"formatting",
+		},
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
+	},
 
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
+	s = {
+		-- Move up and down when selected
+		["J"] = { ":m '>+1<CR>gv=gv" },
+		["K"] = { ":m '<-2<CR>gv=gv" },
+	},
 }
 
 -- more keybinds!
