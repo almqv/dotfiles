0c2b66f99d705c63314853afcafb5f046ec8051c
Elias Almqvist <elalmqvist@gmail.com>
2024-02-19 22:38:42 +0100

    Fixes

--- nvchad/.config/nvim/lua/custom/chadrc.lua
+++ nvchad/.config/nvim/lua/custom/chadrc.lua
@@ -6,7 +6,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "gruvchad",
-  theme_toggle = { "gruvchad", "one_light" },
+  theme_toggle = { "gruvchad", "gruvbox_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
