399563df9be56635ea77a03d38c0ff22bbd4c667
Elias Almqvist <elalmqvist@gmail.com>
2024-06-23 12:05:18 +0200

    stuff

--- nvchad/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvchad/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "ghdl_ls", "hls", "tailwindcss", "pyright" }
+local servers = { "html", "cssls", "tsserver", "clangd", "ghdl_ls", "hls", "tailwindcss", "pyright", "julials" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
