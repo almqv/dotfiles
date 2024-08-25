962e4f02a5bd342f56460bfce78e42c1d9d72eed
Elias Almqvist <elalmqvist@gmail.com>
2024-08-25 16:03:10 -0700

    fix

--- nvchad/.config/nvim/lua/chadrc.lua
+++ nvchad/.config/nvim/lua/chadrc.lua
@@ -1,18 +1,59 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "highlights"
+local highlights = require("highlights")
+
+local custom_logo = {
+	"                                                         ",
+	"                                                         ",
+	"          ________________________________               ",
+	"         /                                '-_            ",
+	"        /      .  |  .                       \\           ",
+	"       /      : \\ | / :                       \\          ",
+	"      /        '-___-'                         \\         ",
+	"     /_________________________________________ \\        ",
+	"          _______| |________________________--''-L       ",
+	"         /       F J                              \\      ",
+	"        /       F   J                              L     ",
+	"       /      :'     ':                            F     ",
+	"      /        '-___-'                            /      ",
+	"     /_________________________________________--'       ",
+	"                                                         ",
+	"                                                         ",
+}
+
+local function split_string(s)
+	local t = {}
+	for str in string.gmatch(s, "([^\n]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
 
 M.ui = {
-  theme = "gruvchad",
-  theme_toggle = { "gruvchad", "blossom_light" },
+	theme = "gruvchad",
+	theme_toggle = { "gruvchad", "blossom_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
+
+	statusline = {
+		theme = "vscode",
+	},
+
+	hl_override = {
+		NvDashAscii = { bg = "blue", fg = "white" },
+	},
+
+	nvdash = {
+		load_on_startup = true,
+		header = custom_logo,
+		width = 100,
+	},
 }
 
 return M
--- nvchad/.config/nvim/lua/options.lua
+++ nvchad/.config/nvim/lua/options.lua
@@ -1,6 +1,6 @@
-require "nvchad.options"
+require("nvchad.options")
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = "both" 
