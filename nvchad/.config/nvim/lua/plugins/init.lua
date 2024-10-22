4b8c4c48d0b8a0d442135e66ee24c71a5a6b4e50
Elias Almqvist <elalmqvist@gmail.com>
2024-10-22 17:06:48 +0200

    remove copilot because it's cringe

--- nvchad/.config/nvim/lua/plugins/init.lua
+++ nvchad/.config/nvim/lua/plugins/init.lua
@@ -104,18 +104,18 @@ local plugins = {
 		end,
 	},
 
-	{
-		"github/copilot.vim",
-		lazy = false,
-		config = function()
-			-- Mapping tab is already used by NvChad
-			vim.g.copilot_no_tab_map = true
-			vim.g.copilot_assume_mapped = true
-			vim.g.copilot_tab_fallback = ""
-			-- The mapping is set to other key, see custom/lua/mappings
-			-- or run <leader>ch to see copilot mapping section
-		end,
-	},
+	-- {
+	-- 	"github/copilot.vim",
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		-- Mapping tab is already used by NvChad
+	-- 		vim.g.copilot_no_tab_map = true
+	-- 		vim.g.copilot_assume_mapped = true
+	-- 		vim.g.copilot_tab_fallback = ""
+	-- 		-- The mapping is set to other key, see custom/lua/mappings
+	-- 		-- or run <leader>ch to see copilot mapping section
+	-- 	end,
+	-- },
 
 	{
 		"nvim-treesitter/nvim-treesitter",
