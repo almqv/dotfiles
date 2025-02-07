9e1669337b10d667e3b85eebbe0ddb80e90750cc
Elias Almqvist <elalmqvist@gmail.com>
2025-02-07 12:16:44 -0800

    cringe

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,23 +1,29 @@
 {
-  description = "Home Manager configuration";
+  description = "Epsilons darwin configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations.elal = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [ ./home.nix ];
+  outputs = inputs: {
+    darwinConfigurations = {
+      "exa" = inputs.darwin.lib.darwinSystem {
+        system = "aarch64-darwin";  # or "x86_64-darwin" for Intel Macs
+        modules = [
+          ./configuration.nix
+          # This module integrates Home Manager:
+          inputs.home-manager.darwinModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.elal = import ./home.nix;
+          }
+        ];
       };
     };
+  };
 }
+
