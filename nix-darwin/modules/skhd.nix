bb9592c3d8c3fc135456dfdc729e4101e81802cf
Elias Almqvist <elalmqvist@gmail.com>
2025-05-26 06:10:06 -0700

    stuff

--- nix-darwin/modules/skhd.nix
+++ nix-darwin/modules/skhd.nix
@@ -43,7 +43,7 @@ in
       cmd - e : yabai -m window --toggle split
 
       # Space Management
-      cmd - r : yabai -m space --rotate 90
+      # cmd - r : yabai -m space --rotate 90
       # cmd - b : yabai -m space --balance
 
       # Screen Navigation
@@ -55,26 +55,26 @@ in
       # cmd + shift - , : yabai -m window --display west; yabai -m display --focus west
 
       # Workspace Navigation
-      cmd - 1 : yabai -m space --focus 1
-      cmd - 2 : yabai -m space --focus 2
-      cmd - 3 : yabai -m space --focus 3
-      cmd - 4 : yabai -m space --focus 4
-      cmd - 5 : yabai -m space --focus 5
-      cmd - 6 : yabai -m space --focus 6
-      cmd - 7 : yabai -m space --focus 7
-      cmd - 8 : yabai -m space --focus 8
-      cmd - 9 : yabai -m space --focus 9
+      # cmd - 1 : yabai -m space --focus 1
+      # cmd - 2 : yabai -m space --focus 2
+      # cmd - 3 : yabai -m space --focus 3
+      # cmd - 4 : yabai -m space --focus 4
+      # cmd - 5 : yabai -m space --focus 5
+      # cmd - 6 : yabai -m space --focus 6
+      # cmd - 7 : yabai -m space --focus 7
+      # cmd - 8 : yabai -m space --focus 8
+      # cmd - 9 : yabai -m space --focus 9
 
       # Send window to workspace
-      cmd + shift - 1 : yabai -m window --space 1
-      cmd + shift - 2 : yabai -m window --space 2
-      cmd + shift - 3 : yabai -m window --space 3
-      cmd + shift - 4 : yabai -m window --space 4
-      cmd + shift - 5 : yabai -m window --space 5
-      cmd + shift - 6 : yabai -m window --space 6
-      cmd + shift - 7 : yabai -m window --space 7
-      cmd + shift - 8 : yabai -m window --space 8
-      cmd + shift - 9 : yabai -m window --space 9
+      # cmd + shift - 1 : yabai -m window --space 1
+      # cmd + shift - 2 : yabai -m window --space 2
+      # cmd + shift - 3 : yabai -m window --space 3
+      # cmd + shift - 4 : yabai -m window --space 4
+      # cmd + shift - 5 : yabai -m window --space 5
+      # cmd + shift - 6 : yabai -m window --space 6
+      # cmd + shift - 7 : yabai -m window --space 7
+      # cmd + shift - 8 : yabai -m window --space 8
+      # cmd + shift - 9 : yabai -m window --space 9
     '';
   };
 }
