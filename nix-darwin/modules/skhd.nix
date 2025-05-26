54a627135067056248a5c017ef6059b673c03f91
Elias Almqvist <elalmqvist@gmail.com>
2025-05-26 00:54:53 -0700

    cringe

--- nix-darwin/modules/skhd.nix
+++ nix-darwin/modules/skhd.nix
@@ -15,68 +15,68 @@ in
     enable = true;
     skhdConfig = ''
       # Applications
-      cmd + return : open -na ${termPath}
-      cmd + b : open -na ${browserPath}
+      alt + t : open -na ${termPath}
+      alt + b : open -na ${browserPath}
 
       # Focus windows
-      cmd + j : yabai -m window --focus south
-      cmd + k : yabai -m window --focus north
-      cmd + h : yabai -m window --focus west
-      cmd + l : yabai -m window --focus east
+      alt + j : yabai -m window --focus south
+      alt + k : yabai -m window --focus north
+      alt + h : yabai -m window --focus west
+      alt + l : yabai -m window --focus east
 
       # Move windows
-      cmd + shift + j : yabai -m window --swap south
-      cmd + shift + k : yabai -m window --swap north
-      cmd + shift + h : yabai -m window --swap west
-      cmd + shift + l : yabai -m window --swap east
+      alt + shift + j : yabai -m window --swap south
+      alt + shift + k : yabai -m window --swap north
+      alt + shift + h : yabai -m window --swap west
+      alt + shift + l : yabai -m window --swap east
 
       # Resize windows
-      cmd + ctrl + j : yabai -m window --resize bottom:0:20
-      cmd + ctrl + k : yabai -m window --resize top:0:20
-      cmd + ctrl + h : yabai -m window --resize left:20:0
-      cmd + ctrl + l : yabai -m window --resize right:20:0
+      alt + ctrl + j : yabai -m window --resize bottom:0:20
+      alt + ctrl + k : yabai -m window --resize top:0:20
+      alt + ctrl + h : yabai -m window --resize left:20:0
+      alt + ctrl + l : yabai -m window --resize right:20:0
 
       # Window States
-      cmd + shift + space : yabai -m window --toggle float
-      cmd + shift + f : yabai -m window --toggle zoom-fullscreen
-      cmd + f : yabai -m window --toggle native-fullscreen
-      cmd + e : yabai -m window --toggle split
+      alt + shift + space : yabai -m window --toggle float
+      alt + shift + f : yabai -m window --toggle zoom-fullscreen
+      alt + f : yabai -m window --toggle native-fullscreen
+      alt + e : yabai -m window --toggle split
 
       # Space Management
-      cmd + r : yabai -m space --rotate 90
-      # cmd + b : yabai -m space --balance
+      alt + r : yabai -m space --rotate 90
+      # alt + b : yabai -m space --balance
 
       # Monitor Management
-      cmd + alt + 1 : yabai -m display --focus 1
-      cmd + alt + 2 : yabai -m display --focus 2
-      cmd + alt + 3 : yabai -m display --focus 3
+      ctrl + alt + 1 : yabai -m display --focus 1
+      ctrl + alt + 2 : yabai -m display --focus 2
+      ctrl + alt + 3 : yabai -m display --focus 3
 
       # Send window to monitor
-      cmd + alt + shift + 1 : yabai -m window --display 1
-      cmd + alt + shift + 2 : yabai -m window --display 2
-      cmd + alt + shift + 3 : yabai -m window --display 3
+      ctrl + alt + shift + 1 : yabai -m window --display 1
+      ctrl + alt + shift + 2 : yabai -m window --display 2
+      ctrl + alt + shift + 3 : yabai -m window --display 3
 
       # Space Navigation
-      cmd + alt + 1 : yabai -m space --focus 1
-      cmd + alt + 2 : yabai -m space --focus 2
-      cmd + alt + 3 : yabai -m space --focus 3
-      cmd + alt + 4 : yabai -m space --focus 4
-      cmd + alt + 5 : yabai -m space --focus 5
-      cmd + alt + 6 : yabai -m space --focus 6
-      cmd + alt + 7 : yabai -m space --focus 7
-      cmd + alt + 8 : yabai -m space --focus 8
-      cmd + alt + 9 : yabai -m space --focus 9
+      ctrl + alt + 1 : yabai -m space --focus 1
+      ctrl + alt + 2 : yabai -m space --focus 2
+      ctrl + alt + 3 : yabai -m space --focus 3
+      ctrl + alt + 4 : yabai -m space --focus 4
+      ctrl + alt + 5 : yabai -m space --focus 5
+      ctrl + alt + 6 : yabai -m space --focus 6
+      ctrl + alt + 7 : yabai -m space --focus 7
+      ctrl + alt + 8 : yabai -m space --focus 8
+      ctrl + alt + 9 : yabai -m space --focus 9
 
       # Send window to space
-      cmd + alt + shift + 1 : yabai -m window --space 1
-      cmd + alt + shift + 2 : yabai -m window --space 2
-      cmd + alt + shift + 3 : yabai -m window --space 3
-      cmd + alt + shift + 4 : yabai -m window --space 4
-      cmd + alt + shift + 5 : yabai -m window --space 5
-      cmd + alt + shift + 6 : yabai -m window --space 6
-      cmd + alt + shift + 7 : yabai -m window --space 7
-      cmd + alt + shift + 8 : yabai -m window --space 8
-      cmd + alt + shift + 9 : yabai -m window --space 9
+      ctrl + alt + shift + 1 : yabai -m window --space 1
+      ctrl + alt + shift + 2 : yabai -m window --space 2
+      ctrl + alt + shift + 3 : yabai -m window --space 3
+      ctrl + alt + shift + 4 : yabai -m window --space 4
+      ctrl + alt + shift + 5 : yabai -m window --space 5
+      ctrl + alt + shift + 6 : yabai -m window --space 6
+      ctrl + alt + shift + 7 : yabai -m window --space 7
+      ctrl + alt + shift + 8 : yabai -m window --space 8
+      ctrl + alt + shift + 9 : yabai -m window --space 9
     '';
   };
 }
