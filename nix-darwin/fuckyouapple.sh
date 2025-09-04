91d6c45049c5cf7ccf61abbb4fd140d5ebb3d7c7
Elias Almqvist <elalmqvist@gmail.com>
2025-09-03 17:00:40 -0700

    fix

--- nix-darwin/fuckyouapple.sh
+++ nix-darwin/fuckyouapple.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+sudo launchctl bootout system /Library/LaunchDaemons/org.nixos.nix-daemon.plist 2>/dev/null
+sudo launchctl bootstrap system /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+sudo launchctl kickstart -k system/org.nixos.nix-daemon
+
 sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .#exa
