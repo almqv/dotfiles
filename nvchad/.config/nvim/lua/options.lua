e19d9d51738446d3ae251b0efa8d4a8fc360489d
Elias Almqvist <elalmqvist@gmail.com>
2025-10-13 11:17:31 -0700

    opts

--- nvchad/.config/nvim/lua/options.lua
+++ nvchad/.config/nvim/lua/options.lua
@@ -1,6 +1,11 @@
-require "nvchad.options"
+require("nvchad.options")
 
 -- add yours here!
 
 local o = vim.o
 o.cursorlineopt = "both"
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+	command = "if mode() != 'c' | checktime | endif",
+})
