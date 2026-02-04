07215bc9ee7e1be2f4096e3a13cfd611037b28bf
Elias Almqvist <elalmqvist@gmail.com>
2026-02-04 10:02:03 -0800

    stuff

--- nvchad/.config/nvim/lua/plugins/init.lua
+++ nvchad/.config/nvim/lua/plugins/init.lua
@@ -1,103 +1,113 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    -- dependencies = {
-    -- 	"JoosepAlviste/nvim-ts-context-commentstring",
-    -- },
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "c",
-        "markdown",
-        "markdown_inline",
-        "prisma",
-        "haskell",
-        "python",
-        "zig",
-        "rust",
-        "vhdl",
-        "scala",
-        "verilog",
-      },
-      indent = {
-        enable = true,
-        -- disable = {
-        --   "python"
-        -- },
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		-- dependencies = {
+		-- 	"JoosepAlviste/nvim-ts-context-commentstring",
+		-- },
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"c",
+				"markdown",
+				"markdown_inline",
+				"prisma",
+				"haskell",
+				"python",
+				"zig",
+				"rust",
+				"vhdl",
+				"scala",
+				"verilog",
+			},
+			indent = {
+				enable = true,
+				-- disable = {
+				--   "python"
+				-- },
+			},
+		},
+	},
 
-  {
-    "stevearc/conform.nvim",
-    --  for users those who want auto-save conform + lazyloading!
-    -- event = "BufWritePre"
-    config = function()
-      local options = {
-        lsp_fallback = true,
+	{
+		"stevearc/conform.nvim",
+		--  for users those who want auto-save conform + lazyloading!
+		-- event = "BufWritePre"
+		config = function()
+			local options = {
+				lsp_fallback = true,
 
-        formatters_by_ft = {
-          -- Divine Intellect Languages
-          lua = { "stylua" },
+				formatters_by_ft = {
+					-- Divine Intellect Languages
+					lua = { "stylua" },
 
-          -- rust
-          rust = { "rustfmt" },
+					-- rust
+					rust = { "rustfmt" },
 
-          -- Web Dev shit
-          javascript = { "prettier" },
-          typescript = { "prettier" },
-          javascriptreact = { "prettier" },
-          typescriptreact = { "prettier" },
-          css = { "prettier" },
-          scss = { "prettier" },
-          html = { "prettier" },
+					-- Web Dev shit
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					css = { "prettier" },
+					scss = { "prettier" },
+					html = { "prettier" },
 
-          -- Data Science & AI stuff
-          python = { "black" },
+					-- Data Science & AI stuff
+					python = { "black" },
 
-          sh = { "shfmt" },
+					sh = { "shfmt" },
 
-          -- Joke langs
-          haskell = { "stylish-haskell" },
+					-- Joke langs
+					haskell = { "stylish-haskell" },
 
-          -- systemverilog
-          verilog = { "verible" },
-        },
-      }
-      require("conform").setup(options)
-    end,
-  },
+					-- systemverilog
+					verilog = { "verible" },
+				},
+			}
+			require("conform").setup(options)
+		end,
+	},
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	-- test new blink
+	-- { import = "nvchad.blink.lazyspec" },
+
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
